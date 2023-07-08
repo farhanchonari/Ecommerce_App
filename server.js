63400592a0acbda03319ad091562a6359fd38b48
Farhan Chonari <111281197+farhanchonari@users.noreply.github.com>
2023-07-09 01:35:36 +0530

    Updated dirname issue

--- server.js
+++ server.js
@@ -8,8 +8,12 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
+import {fileURLToPath} from 'url';
 //configure env
 dotenv.config();
+//esmodule fix
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 //Database config
 connectDB();
 //rest object
